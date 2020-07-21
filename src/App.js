ceef41d8993e76bf1b004dd5e0fced168629ac20
Zoran Markovic <zoran.markovic@netcentric.biz>
2020-07-21 10:41:40 +0200

    use Transition iside Modal component

--- src/App.js
+++ src/App.js
@@ -13,70 +13,61 @@ class App extends Component {
 	};
 
 	showModal = () => {
-		this.setState({
-			modalIsOpen: true,
-		});
+		this.setState({ modalIsOpen: true });
 	};
 
 	closeModal = () => {
-		this.setState({
-			modalIsOpen: false,
-		});
+		this.setState({ modalIsOpen: false });
 	};
 
 	render() {
 		return (
 			<div className="App">
 				<h1>React Animations</h1>
-				<section>
-					<button
-						className="Button"
-						onClick={() =>
-							this.setState((prevState) => ({
-								showBlock: !prevState.showBlock,
-							}))
-						}
-					>
-						Toggle Block
-					</button>
-					<p></p>
-					<Transition
-						in={this.state.showBlock}
-						timeout={500}
-						mountOnEnter
-						unmountOnExit
-					>
-						{(state) => (
-							<div
-								style={{
-									backgroundColor: 'red',
-									width: 100,
-									height: 100,
-									margin: 'auto',
-									transition: 'opacity 1s ease-out',
-									opacity: (state === 'exiting' ? 0 : 1),
-								}}
-							>
-								I'm a fade Transition!
-							</div>
-						)}
-					</Transition>
-				</section>
-				<section>
-					<p></p>
+				<button
+					className="Button"
+					onClick={() =>
+						this.setState((prevState) => ({
+							showBlock: !prevState.showBlock,
+						}))
+					}
+				>
+					Toggle
+				</button>
+				<br />
+				<Transition
+					in={this.state.showBlock}
+					timeout={1000}
+					mountOnEnter
+					unmountOnExit
+				>
+					{(state) => (
+						<div
+							style={{
+								backgroundColor: 'red',
+								width: 100,
+								height: 100,
+								margin: 'auto',
+								transition: 'opacity 1s ease-out',
+								opacity: state === 'exiting' ? 0 : 1,
+							}}
+						/>
+					)}
+				</Transition>
+				{this.state.modalIsOpen ? (
 					<Modal
 						show={this.state.modalIsOpen}
 						closed={this.closeModal}
 					/>
+				) : null}
+				{this.state.modalIsOpen ? (
 					<Backdrop show={this.state.modalIsOpen} />
-					<button className="Button" onClick={this.showModal}>
-						Open Modal
-					</button>
-				</section>
-				<section>
-					<h3>Animating Lists</h3>
-					<List />
-				</section>
+				) : null}
+				<button className="Button" onClick={this.showModal}>
+					Open Modal
+				</button>
+				<h3>Animating Lists</h3>
+				<List />
 			</div>
 		);
 	}
